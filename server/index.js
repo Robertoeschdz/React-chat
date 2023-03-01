d3ab04ec3b334db01462c294ada86f71cf082ae5
Robero <robertoeschdz@gmail.com>
2023-03-01 03:56:15 -0600

    First commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import morgan from "morgan";
+import { Server as SocketServer} from "socket.io";
+import http from 'http'
+import cors from 'cors'
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const server = http.createServer(app)
+const io = new SocketServer(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+    }
+})
+
+app.use(cors())
+app.use(morgan('dev'))
+
+io.on('connection', (socket) => {    
+    
+    socket.on('message', (data) => {
+        socket.broadcast.emit('message', {
+            body: data.message,
+            from: data.username
+        })
+    })
+})
+
+server.listen(PORT, () => {
+    console.log('Server on port', PORT)
+})
